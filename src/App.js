6d39808b8f1efe328852e15b0f66d9ce2f223755
xiaxiazheng <13075218875@163.com>
2019-09-07 18:04:27 +0800

    小 demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Comp1 from './Comp1.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+      type: null
+    };
+  }
+
+  handleAChange = (value) => {
+    this.setState({
+      value: value,
+      type: 'a'
+    });
+  }
+
+  handleBChange = (value) => {
+    this.setState({
+      value: value,
+      type: 'b'
+    });
+  }
+
+  render() {
+    let a = this.state.type === 'b' ? (parseInt(this.state.value) + 1) : this.state.value;
+    let b = this.state.type === 'a' ? (parseInt(this.state.value) - 1) : this.state.value;
+    return (
+      <div className="App">
+        <Comp1 type="a" value={a} onChange={this.handleAChange}></Comp1>
+        <Comp1 type="b" value={b} onChange={this.handleBChange}></Comp1>
+      </div>
+    );
+  }
 }
 
 export default App;
