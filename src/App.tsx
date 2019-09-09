43b7c6f659dde81aecbe8807e291f73bf4f44df2
xiaxiazheng <13075218875@163.com>
2019-09-09 14:37:37 +0800

    add ts

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        666
+      </header>
+    </div>
+  );
+}
+
+export default App;
