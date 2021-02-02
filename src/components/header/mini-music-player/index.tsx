322c415f954d1e2136b900b49836eb662187b34a
xiaxiazheng <13075218875@163.com>
2021-02-02 10:08:34 +0800

    feat: 重构了音乐播放器的拖放，使用 H5 的 drag 事件

--- src/components/header/mini-music-player/index.tsx
+++ src/components/header/mini-music-player/index.tsx
@@ -3,48 +3,37 @@ import styles from "./index.module.scss";
 import MusicPlayer from '@/components/music-player'
 
 const MiniMusicPlayer: React.FC = () => {
-  const circle = useRef(null);
-
-  const [isDrog, setIsDrog] = useState(false);
-  const [downX, setDownX] = useState<any>();
-  const [downY, setDownY] = useState<any>();
+  const [offsetX, setOffsetX] = useState<any>();
+  const [offsetY, setOffsetY] = useState<any>();
   const [left, setLeft] = useState(
     Number(localStorage.getItem("musicLeft") || 0)
   );
   const [top, setTop] = useState(Number(localStorage.getItem("musicTop") || 0));
 
-  const down = (e: any) => {
-    e.preventDefault();
-    setIsDrog(true);
-    const dom: any = circle.current;
-    setDownX(e.clientX - dom.offsetLeft);
-    setDownY(e.clientY - dom.offsetTop);
-  };
-
-  const move = (e: any) => {
-    e.preventDefault();
-    if (isDrog) {
-      setLeft(e.clientX - downX);
-      setTop(e.clientY - downY);
-    }
-  };
-
-  const up = (ev: any) => {
-    ev.preventDefault();
-    setIsDrog(false);
-    localStorage.setItem("musicLeft", String(left));
-    localStorage.setItem("musicTop", String(top));
-  };
-
   return (
     <div
-      ref={circle}
       className={`${styles.miniMusicPlayer}`}
-      draggable={true}
-      onMouseDown={down}
-      onMouseMove={move}
-      onMouseUp={up}
-      onMouseLeave={up}
+      draggable
+      onDragStart={(e) => {
+        setOffsetX(e.nativeEvent.offsetX)
+        setOffsetY(e.nativeEvent.offsetY)
+      }}
+      onDrag={(e) => {
+        setLeft(e.nativeEvent.x - offsetX)
+        setTop(e.nativeEvent.y - offsetY)
+      }}
+      onDragEnd={(e) => {
+        const newLeft = e.nativeEvent.x - offsetX
+        const newTop = e.nativeEvent.y - offsetY
+        setLeft(newLeft)
+        setTop(newTop)
+        localStorage.setItem("musicLeft", String(newLeft));
+        localStorage.setItem("musicTop", String(newTop));
+      }}
+      onDragOver={(e) => {
+        e.dataTransfer.dropEffect = 'move';
+        e.preventDefault()
+      }}
       style={{
         left: left,
         top: top,
