da9504770aec2005db9a7cdef1b393e4b2b738af
xiaxiazheng <13075218875@163.com>
2021-02-24 15:15:41 +0800

    feat: 新增本地调试 static-server 的启动命令，npm run startLocalStatic

--- src/env_config.tsx
+++ src/env_config.tsx
@@ -3,12 +3,18 @@ let host: string = "https://www.xiaxiazheng.cn";
 let url: string = `${host}`;
 export const cdnUrl = 'http://cdn.xiaxiazheng.cn';
 // 静态资源服务器
-const staticurl = `${host}:2333`
+let staticurl = `${host}:2333`
 
 // 接本地后台 300 接口
 if (process.env.REACT_APP_IS_LocalHost && process.env.REACT_APP_IS_LocalHost === "yes") {
   host = "http://localhost";
   url = `${host}:300`;
+}
+
+// 接本地静态资源服务 2333 接口
+if (process.env.REACT_APP_IS_LOCALSTATIC && process.env.REACT_APP_IS_LOCALSTATIC === "yes") {
+  host = "http://localhost";
+  staticurl = `${host}:2333`;
 }
 
 export const isDev = process.env.NODE_ENV === 'development';
