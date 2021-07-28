f8987be231bf18a56ac4b5aaeae48553356152e8
xiaxiazheng <13075218875@163.com>
2021-07-28 23:31:18 +0800

    feat: 修改 yarn deploy 命令，用 deploy.sh 脚本进行打包

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,17 @@
 yarn build
 
+cd ../blog-deploy
+
+git pull
+
+rm -rf *
+
+cp -r ../reactblog/build/* ./
+
 git add .
 
 git commit -m "feat: 更新前端代码"
 
-cd ../blog-deploy
+git push
 
-git pull
+exec /bin/bash
