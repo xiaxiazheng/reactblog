f50693ff020039dd1385874e841a90cb44c2d3ab
zhengyanbin <zhengyanbin@bytedance.com>
2022-06-07 00:03:32 +0800

    feat: 新增预设脚本

--- src/views/cmd/index.tsx
+++ src/views/cmd/index.tsx
@@ -6,9 +6,10 @@ import classnames from "classnames";
 import { UserContext } from "@/context/UserContext";
 import { withRouter, RouteComponentProps } from "react-router-dom";
 import beian from "@/assets/beian.png";
-import { Button, Input, Spin } from "antd";
+import { Button, Input, message, Space, Spin } from "antd";
 import { exec } from "@/client/CmdHelper";
 import { useCtrlSHooks } from "./useCtrlSHook";
+import { addNote, getNoteList } from "@/client/NoteHelper";
 
 const { TextArea } = Input;
 
@@ -43,6 +44,41 @@ const CMD: React.FC<RouteComponentProps> = (props) => {
 
     useCtrlSHooks(submit);
 
+    const [list, setList] = useState<any>([]);
+    const getScript = async () => {
+        setLoading(true);
+
+        const params: any = {
+            pageNo: 1,
+            pageSize: 100,
+            category: "脚本",
+            keyword: "",
+        };
+        const res = await getNoteList(params);
+        if (res) {
+            setList(res?.data?.list || []);
+        }
+        setLoading(false);
+    };
+    useEffect(() => {
+        getScript();
+        submit();
+    }, []);
+
+    const saveScript = async () => {
+        const params = {
+            category: "脚本",
+            note: cmd,
+        };
+        const res = await addNote(params);
+        if (res) {
+            message.success("保存脚本成功");
+            getScript();
+        } else {
+            message.error("保存脚本失败");
+        }
+    };
+
     return (
         <div className={styles.cmd}>
             <TextArea
@@ -51,13 +87,30 @@ const CMD: React.FC<RouteComponentProps> = (props) => {
                 onChange={(e) => setCmd(e.target.value)}
                 rows={8}
             />
-            <Button type="primary" onClick={() => submit()}>
-                执行
-            </Button>
+            <Space size={10}>
+                <Button type="primary" onClick={() => submit()}>
+                    执行
+                </Button>
+                <Button onClick={() => saveScript()}>保存</Button>
+            </Space>
             <div style={{ marginTop: 20 }}>结果：</div>
             <Spin spinning={loading}>
                 <div className={styles.result}>{result}</div>
             </Spin>
+            <div style={{ marginTop: 20 }}>预设脚本：</div>
+            <div className={styles.script}>
+                {list?.map((item: any) => {
+                    return (
+                        <div
+                            className={styles.scriptItem}
+                            key={item.note_id}
+                            onClick={() => setCmd(item.note)}
+                        >
+                            {item.note}
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
