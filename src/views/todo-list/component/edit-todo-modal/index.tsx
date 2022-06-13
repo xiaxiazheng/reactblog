99dc392f6f28b550af31b4b665d5a8e347f5f6e5
zhengyanbin <zhengyanbin@bytedance.com>
2022-06-13 10:30:05 +0800

    feat: 完善todo删除逻辑

--- src/views/todo-list/component/edit-todo-modal/index.tsx
+++ src/views/todo-list/component/edit-todo-modal/index.tsx
@@ -59,7 +59,7 @@ const EditTodoModal: React.FC<EditTodoModalType> = (props) => {
                 color: formData.color,
                 category: formData.category,
                 other_id: formData.other_id || "",
-                doing: formData.doing || "0"
+                doing: formData.doing || "0",
             };
             const res = await addTodoItem(req);
             if (res) {
@@ -91,7 +91,7 @@ const EditTodoModal: React.FC<EditTodoModalType> = (props) => {
                 color: formData.color,
                 category: formData.category,
                 other_id: formData.other_id || "",
-                doing: formData.doing || "0"
+                doing: formData.doing || "0",
             };
             const res = await editTodoItem(req);
             if (res) {
@@ -194,7 +194,9 @@ const EditTodoModal: React.FC<EditTodoModalType> = (props) => {
                                         danger
                                         disabled={
                                             activeTodo?.imgList &&
-                                            activeTodo.imgList.length !== 0
+                                            activeTodo.imgList.length !== 0 &&
+                                            activeTodo?.fileList &&
+                                            activeTodo.fileList.length !== 0
                                         }
                                     >
                                         删除
