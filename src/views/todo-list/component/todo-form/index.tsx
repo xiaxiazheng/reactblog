e8a1f7035b6675ce6e9d000ad0c853b9d43ee0c1
zyb <zhengyanbin@bytedance.com>
2023-02-24 12:58:15 +0800

    feat: todo form的title输入框,调成默认一行最多两行

--- src/views/todo-list/component/todo-form/index.tsx
+++ src/views/todo-list/component/todo-form/index.tsx
@@ -109,11 +109,12 @@ const TodoForm: React.FC<Props> = (props) => {
             onFieldsChange={isFieldsChange}
         >
             <Form.Item name="name" label="名称" rules={[{ required: true }]}>
-                <Input
+                <Input.TextArea
                     placeholder="尽量的量化，有具体的完成指标，任务尽量细致且易完成"
                     onPressEnter={onOk}
                     autoFocus={true}
                     allowClear
+                    autoSize={{ minRows: 1, maxRows: 2 }}
                 />
             </Form.Item>
             <Form.Item
@@ -208,7 +209,7 @@ const TodoForm: React.FC<Props> = (props) => {
                     <Radio.Button value={2}>待办池</Radio.Button>
                 </Radio.Group>
             </Form.Item>
-            <Form.Item label="特殊状态" style={{ marginBottom: 0}}>
+            <Form.Item label="特殊状态" style={{ marginBottom: 0 }}>
                 <Space>
                     <Form.Item
                         name="doing"
