0e9ad5bf164f48ced77535adf740c2e9aa88be94
xiaxiazheng <13075218875@163.com>
2024-03-31 18:25:57 +0800

    feat: 完成了一个我自己很满意的全局搜索里的时间范围筛选器

--- src/views/todo-list/component/global-search/filter.tsx
+++ src/views/todo-list/component/global-search/filter.tsx
@@ -16,7 +16,7 @@ const Filter = () => {
     const activeColor = useSelector(
         (state: RootState) => state.filter.activeColor
     );
-    const startEndTime = useSelector(
+    const startEndTime: Array<dayjs.Dayjs> = useSelector(
         (state: RootState) => state.filter.startEndTime
     );
     const isTarget = useSelector((state: RootState) => state.filter.isTarget);
@@ -44,6 +44,66 @@ const Filter = () => {
         半年内: getTimeRange(0, 6, "month"),
         一年内: getTimeRange(0, 12, "month"),
         一年前: getTimeRange(1, 10, "year"),
+    };
+
+    const [timeType, setTimeType] = useState<"month" | "day" | "year">("day");
+
+    const handleStarEndTime = (val: any) => {
+        setStartEndTime(val);
+    };
+
+    const handleAddSubtractTime = (
+        operate: "add" | "subtract",
+        type: "start" | "end"
+    ) => {
+        let start = startEndTime[0];
+        let end = startEndTime[1];
+        if (type === "start") {
+            start =
+                operate === "add"
+                    ? start.add(1, timeType)
+                    : start.subtract(1, timeType);
+        } else {
+            end =
+                operate === "add"
+                    ? end.add(1, timeType)
+                    : end.subtract(1, timeType);
+        }
+
+        setStartEndTime([start, end]);
+    };
+
+    const handleTimeTypeChange = (newVal: any) => {
+        if (!startEndTime) {
+            setTimeType(newVal);
+            return;
+        }
+        const oldVal = timeType;
+        let newStarEndTime = startEndTime;
+        const start = startEndTime[0];
+        const end = startEndTime[1];
+        if (newVal === "year") {
+            newStarEndTime = [start.startOf("year"), end.endOf("year")];
+        }
+        if (newVal === "month") {
+            if (oldVal === "day") {
+                newStarEndTime = [start.startOf("month"), end.endOf("month")];
+            }
+            if (oldVal === "year") {
+                newStarEndTime = [start.startOf("year"), end.endOf("year")];
+            }
+        }
+        if (newVal === "day") {
+            if (oldVal === "month") {
+                newStarEndTime = [start.startOf("month"), end.endOf("month")];
+            }
+
+            if (oldVal === "year") {
+                newStarEndTime = [start.startOf("year"), end.endOf("year")];
+            }
+        }
+        setStartEndTime(newStarEndTime);
+        setTimeType(newVal);
     };
 
     return (
@@ -77,69 +137,128 @@ const Filter = () => {
                 </Radio.Group>
             </div>
             <div>
-                <span>时间：</span>
-                {startEndTime?.[0] && (
-                    <>
-                        <Button
-                            icon={<MinusOutlined />}
-                            onClick={() =>
-                                setStartEndTime([
-                                    dayjs(startEndTime?.[0]).subtract(1, "d"),
-                                    startEndTime?.[1],
-                                ])
-                            }
+                <div>
+                    <span>时间：</span>
+                    {startEndTime?.[0] && (
+                        <>
+                            <Button
+                                icon={<MinusOutlined />}
+                                onClick={() =>
+                                    handleAddSubtractTime("subtract", "start")
+                                }
+                            />
+                            <Button
+                                icon={<PlusOutlined />}
+                                onClick={() =>
+                                    handleAddSubtractTime("add", "start")
+                                }
+                            />
+                        </>
+                    )}
+                    {timeType === "day" && (
+                        <DatePicker.RangePicker
+                            style={{ width: 250 }}
+                            value={startEndTime as any}
+                            onChange={(val) => handleStarEndTime(val)}
+                            placeholder={["开始时间", "结束时间"]}
+                            ranges={{
+                                Today: [dayjs(), dayjs()],
+                                "这周至今": [
+                                    dayjs().startOf("week"),
+                                    dayjs(),
+                                ],
+                                "一周内": [
+                                    dayjs().subtract(1, 'week'),
+                                    dayjs(),
+                                ],
+                                "这月至今": [
+                                    dayjs().startOf("month"),
+                                    dayjs(),
+                                ],
+                                "今年至今": [
+                                    dayjs().startOf("year"),
+                                    dayjs(),
+                                ],
+                            }}
                         />
-                        <Button
-                            icon={<PlusOutlined />}
-                            onClick={() =>
-                                setStartEndTime([
-                                    dayjs(startEndTime?.[0]).add(1, "d"),
-                                    startEndTime?.[1],
-                                ])
-                            }
+                    )}
+                    {timeType === "month" && (
+                        <DatePicker.RangePicker
+                            style={{ width: 200 }}
+                            picker="month"
+                            value={startEndTime as any}
+                            onChange={(val) => handleStarEndTime(val)}
+                            placeholder={["开始时间", "结束时间"]}
+                            ranges={{
+                                "这个月": [
+                                    dayjs().startOf("month"),
+                                    dayjs().endOf("month"),
+                                ],
+                                "上个月": [
+                                    dayjs().startOf("month").subtract(1, 'month'),
+                                    dayjs().endOf("month").subtract(1, 'month'),
+                                ],
+                                "上上个月": [
+                                    dayjs().startOf("month").subtract(2, 'month'),
+                                    dayjs().endOf("month").subtract(2, 'month'),
+                                ],
+                            }}
                         />
-                    </>
-                )}
-                <DatePicker.RangePicker
-                    value={startEndTime}
-                    onChange={(val) => setStartEndTime(val)}
-                    placeholder={["开始时间", "结束时间"]}
-                />
-                {startEndTime?.[1] && (
-                    <>
-                        <Button
-                            icon={<MinusOutlined />}
-                            onClick={() =>
-                                setStartEndTime([
-                                    startEndTime?.[0],
-                                    dayjs(startEndTime?.[1]).subtract(1, "d"),
-                                ])
-                            }
+                    )}
+                    {timeType === "year" && (
+                        <DatePicker.RangePicker
+                            style={{ width: 200 }}
+                            picker="year"
+                            value={startEndTime as any}
+                            onChange={(val) => handleStarEndTime(val)}
+                            placeholder={["开始时间", "结束时间"]}
+                            ranges={{
+                                "今年": [
+                                    dayjs().startOf("year"),
+                                    dayjs().endOf("year"),
+                                ],
+                                "去年": [
+                                    dayjs().startOf("year").subtract(1, 'y'),
+                                    dayjs().endOf("year").subtract(1, 'y'),
+                                ],
+                                "前年": [
+                                    dayjs().startOf("year").subtract(2, 'y'),
+                                    dayjs().endOf("year").subtract(2, 'y'),
+                                ],
+                            }}
                         />
-                        <Button
-                            icon={<PlusOutlined />}
-                            onClick={() =>
-                                setStartEndTime([
-                                    startEndTime?.[0],
-                                    dayjs(startEndTime?.[1]).add(1, "d"),
-                                ])
-                            }
-                        />
-                    </>
-                )}
-
-                {Object.keys(timeRange).map((item) => (
-                    <Button
-                        type="text"
-                        key={item}
-                        className={styles.btn}
-                        onClick={() =>
-                            setStartEndTime(timeRange[item].reverse())
-                        }
+                    )}
+                    {startEndTime?.[1] && (
+                        <>
+                            <Button
+                                icon={<MinusOutlined />}
+                                onClick={() => {
+                                    handleAddSubtractTime("subtract", "end");
+                                }}
+                            />
+                            <Button
+                                icon={<PlusOutlined />}
+                                onClick={() => {
+                                    handleAddSubtractTime("add", "end");
+                                }}
+                            />
+                        </>
+                    )}
+                </div>
+                <Space style={{ marginTop: 8, paddingLeft: 42 }}>
+                    <Radio.Group
+                        value={timeType}
+                        onChange={(val) => {
+                            handleTimeTypeChange(val.target.value);
+                        }}
                     >
-                        {item}
-                    </Button>
-                ))}
+                        <Radio.Button value={"year"}>年</Radio.Button>
+                        <Radio.Button value={"month"}>月</Radio.Button>
+                        <Radio.Button value={"day"}>日</Radio.Button>
+                    </Radio.Group>
+                    <span>今天是 {dayjs().format('YYYY-MM-DD')}</span>
+                </Space>
+                
             </div>
 
             <div>
@@ -178,7 +297,7 @@ const Filter = () => {
                         value={isHabit}
                         onChange={(val) =>
                             handleSpecialStatus({
-                                type: "isTarget",
+                                type: "isHabit",
                                 status: val,
                             })
                         }
