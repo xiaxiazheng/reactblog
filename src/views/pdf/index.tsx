362e2aded17af2e90c5aefd0ca07a2c8cbbc10a0
xiaxiazheng <13075218875@163.com>
2024-08-04 16:44:50 +0800

    chore: chore

--- src/views/pdf/index.tsx
+++ src/views/pdf/index.tsx
@@ -10,50 +10,55 @@ import useDocumentTitle from "@/hooks/useDocumentTitle";
 interface IPDF extends RouteComponentProps {}
 
 const PDF: React.FC<IPDF> = (props) => {
-  const { state } = props.location;
+    const { state } = props.location;
 
-  const [blogType, setBLogType] = useState<"richtext" | "markdown" | "">("");
-  const [blogdata, setBlogdata] = useState<any>();
+    const [blogType, setBLogType] = useState<"richtext" | "markdown" | "">("");
+    const [blogdata, setBlogdata] = useState<any>();
 
-  useDocumentTitle(`打印：${blogdata?.title || ''}`)
+    useDocumentTitle(`打印：${blogdata?.title || ""}`);
 
-  useEffect(() => {
-    setTimeout(() => {
-      message.info('Alt + ← 返回', 5)
-    }, 3000)
-  }, [])
+    useEffect(() => {
+        setTimeout(() => {
+            message.info("Alt + ← 返回", 5);
+        }, 3000);
+    }, []);
 
-  useEffect(() => {
-    let myState: any = state;
-    if (myState && myState.type) {
-      setBLogType(myState.type);
-      setBlogdata(myState.blogData);
-    }
-  }, [state]);
+    useEffect(() => {
+        let myState: any = state;
+        if (myState && myState.type) {
+            setBLogType(myState.type);
+            setBlogdata(myState.blogData);
+        }
+    }, [state]);
 
-  useEffect(() => {
-    if (blogType !== "") {
-      // 这里在下一个宏任务才执行打印，因为 MarkdownShow 需要拿到 blogcont 之后再做一次转换才能渲染
-      setTimeout(() => {
-        window.print();
-      }, 2000)
-    }
-  }, [blogType]);
+    useEffect(() => {
+        if (blogType !== "") {
+            // 这里在下一个宏任务才执行打印，因为 MarkdownShow 需要拿到 blogcont 之后再做一次转换才能渲染
+            setTimeout(() => {
+                window.print();
+            }, 2000);
+        }
+    }, [blogType]);
 
-  return (
-    <div className={`${styles.pdf} ScrollBar`}>
-      {/* 打印 markdown */}
-      {blogType === "markdown" && <MarkdownShow blogcont={blogdata?.blogcont} style={{ padding: '0 120px 0 0' }} />}
-      {/* 打印富文本 */}
-      {blogType === "richtext" && (
-        <div className={rhStyles.blogcontShow}>
-          <div className={rhStyles.title}>{blogdata?.title}</div>
-          <div className={rhStyles.author}>{blogdata?.author}</div>
-          <RichtextShow blogcont={blogdata?.blogcont} />
+    return (
+        <div className={`${styles.pdf} ScrollBar`}>
+            {/* 打印 markdown */}
+            {blogType === "markdown" && (
+                <MarkdownShow
+                    blogcont={blogdata?.blogcont}
+                    style={{ padding: "0 120px 0 0" }}
+                />
+            )}
+            {/* 打印富文本 */}
+            {blogType === "richtext" && (
+                <div className={rhStyles.blogcontShow}>
+                    <div className={rhStyles.title}>{blogdata?.title}</div>
+                    <div className={rhStyles.author}>{blogdata?.author}</div>
+                    <RichtextShow blogcont={blogdata?.blogcont} />
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default withRouter(PDF);
