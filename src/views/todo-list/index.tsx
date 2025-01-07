c324499a4ff314dd9e34dfa43a6e977abb067c32
zhengyanbin <zhengyanbin@bytedance.com>
2025-01-07 11:36:10 +0800

    fix: fix error

--- src/views/todo-list/index.tsx
+++ src/views/todo-list/index.tsx
@@ -144,7 +144,7 @@ const TodoList: React.FC = () => {
                             <DoneList
                                 title={
                                     <>
-                                        {todoNameMap["done"]}
+                                        {todoNameMap?.["done"]}
                                         <RenderTodoDescriptionIcon
                                             title={todoDescriptionMap?.["done"]}
                                         />{" "}
