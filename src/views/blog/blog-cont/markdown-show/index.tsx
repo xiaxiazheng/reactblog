9f5650679cf6598bbc8d11036826191ba203aa48
zhengyanbin <zhengyanbin@bytedance.com>
2025-03-06 16:24:40 +0800

    feat: add highlight keyword in markdownShow

--- src/views/blog/blog-cont/markdown-show/index.tsx
+++ src/views/blog/blog-cont/markdown-show/index.tsx
@@ -7,10 +7,27 @@ import "highlight.js/styles/vs2015.css";
 interface PropsType {
     blogcont: string | undefined;
     style?: React.CSSProperties;
+    keyword?: string; // 用于高亮的关键字
 }
 
 const MarkdownShow = React.forwardRef<HTMLInputElement, PropsType>((props, ref) => {
-    const { blogcont, style } = props;
+    const { blogcont, style, keyword } = props;
+    
+    function keywordHighlightPlugin(md: any, options: any) {
+        const { keyword } = options;
+        const originalTextRenderer = md.renderer.rules.text; // 原本的默认处理也只是 escapeHtml 而已
+
+        md.renderer.rules.text = (tokens: any, idx: any, options: any, env: any, self: any) => {
+            const text = originalTextRenderer(tokens, idx, options, env, self);
+            if (keyword) {
+                // 使用正则表达式替换关键词
+                return text.replace(new RegExp(keyword, 'gi'), (match: any) => {
+                    return '<span class="keyword">' + match + '</span>';
+                });
+            }
+            return text;
+        };
+    }
 
     const md = markdownIt({
         highlight: function (str: string, lang: string) {
@@ -23,6 +40,8 @@ const MarkdownShow = React.forwardRef<HTMLInputElement, PropsType>((props, ref) 
             }
             return '<pre><code class="hljs">' + md.utils.escapeHtml(str) + '</code></pre>';
         }
+    }).use(keywordHighlightPlugin, {
+        keyword
     });
 
     return (
