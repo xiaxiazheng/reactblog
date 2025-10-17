7bbd93d4a0e84d8f6a568f8e72da317de46c6c01
xiaxiazheng <13075218875@163.com>
2025-10-17 22:07:57 +0800

    fix: try to fix dayjs problem

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,21 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.d';
 
+import dayjs from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+import localeData from 'dayjs/plugin/localeData'
+import weekday from 'dayjs/plugin/weekday'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+import weekYear from 'dayjs/plugin/weekYear'
+
+dayjs.extend(customParseFormat)
+dayjs.extend(advancedFormat)
+dayjs.extend(weekday)
+dayjs.extend(localeData)
+dayjs.extend(weekOfYear)
+dayjs.extend(weekYear)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
